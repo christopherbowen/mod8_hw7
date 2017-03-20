d56a0568134313a4657268c85472006fc789a4ea
christopher bowen <christopherbowen@mail.weber.edu>
2017-03-20 19:09:26 +0000

    initial commit

---
+++ Duke_task2.c
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Duke_task2.c
+ *    Description:  display a random array and the highest and lowest value
+ *
+ *        Created:  03/20/2017 06:40:07 PM
+ *         Author:  Christopher Bowen (), christopherbowen@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+/* Function Prototypes */
+void MaxMin(int numvals, int vals[],int* min, int* max);
+/* Main Program */
+int main(int argc, char *argv[])
+{
+	int value[];
+	//int min, max;
+	//int numvals;
+	printf("%d\n", rand()%100);
+	return 0;
+}
+
+
+/* Function Defenitions */
+void MaxMin(int numvals, int vals[],int* min, int* max)
+{
+	
+}
+
+
