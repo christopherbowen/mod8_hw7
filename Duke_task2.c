f949c8d556d53579c20cb810bfa395d76eac19fe
christopher bowen <christopherbowen@mail.weber.edu>
2017-03-21 04:41:22 +0000

    second commit

--- Duke_task2.c
+++ Duke_task2.c
@@ -10,15 +10,25 @@
  */
 #include <stdio.h>		/* For Standard I/O */
 #include <stdlib.h>
+#define TOTAL 10
 /* Function Prototypes */
 void MaxMin(int numvals, int vals[],int* min, int* max);
 /* Main Program */
 int main(int argc, char *argv[])
 {
-	int value[];
-	//int min, max;
-	//int numvals;
-	printf("%d\n", rand()%100);
+	int value[TOTAL];
+	int min, max;
+	int numvals=0;
+	printf("Generating random list\n");
+	printf("This list is: \n");
+	for(int i = 0; i < TOTAL; i++)
+	{
+		value[i]= rand()%100;
+		printf("%d ", value[i]);
+	}
+	MaxMin(numvals, value, &min, &max);
+	printf("\nThe maximum value is: %d\n", max);
+	printf("The minimum value is: %d\n", min);
 	return 0;
 }
 
@@ -26,7 +36,19 @@ int main(int argc, char *argv[])
 /* Function Defenitions */
 void MaxMin(int numvals, int vals[],int* min, int* max)
 {
-	
+	for(int i = 0; i<TOTAL; i++)
+	{
+		int numval = vals[i];
+		if(numval < vals[i+1])
+		{
+			numval= *min;
+		}
+		else
+		{
+			numval= *max;
+		}
+	}
+	return;
 }
 
 
