b1ad957a144af2b28649057328ea0946bb242ebc
Tysen Bair <tysenbair@mail.weber.edu>
2017-03-21 04:04:38 +0000

    first

--- Duke_Task1.c
+++ Duke_Task1.c
@@ -3,7 +3,7 @@
  *
  *       Filename:  Duke_Task1.c
  *       	Usage:  ./Duke_Task1.c
- *    Description:  
+ *    Description:  mod8_hw7-task1  
  *
  *        Version:  1.0
  *        Created:  03/21/2017 02:47:21 AM
@@ -14,16 +14,44 @@
 #include <stdio.h>		/* For Standard I/O */
 #include <stdlib.h>
 
+#define NUM 5
 /* Function Prototypes */
+void Copy_Arry(double ar1[], const double ar2[],int n);
+void Copy_Ptr(double *ar1, const double *ar2, int n);
+void Copy_Ptrs(double *ar2, const double *start, const double *end);
+void Show_Arr(const double ar[], double n);
 
 /* Main Program */
 int main(int argc, char *argv[])
 {
-
+	double source [NUM] = {1.5, 2.5, 3.5, 5.8, 6.7};
+	double target1[NUM];
+	double target2[NUM];
+	double target3[NUM];
+	
 	return 0;
 }
 
 
 /* Function Defenitions */
+void Copy_Arry(double ar1[], const double ar2[],int n)
+{
 
+	return;
+}
+void Copy_Ptr(double *ar1, const double *ar2, int n)
+{
+
+	return;
+}
+void Copy_Ptrs(double *ar2, const double *start, const double *end)
+{
+
+	return;
+}
+void Show_Arr(const double ar[], double n)
+{
+
+	return;
+}
 
