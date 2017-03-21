750f7baf281f523edda666b39439140151d6dbc5
Tysen Bair <tysenbair@mail.weber.edu>
2017-03-21 02:49:00 +0000

    Initial

---
+++ Duke_Task1.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Duke_Task1.c
+ *       	Usage:  ./Duke_Task1.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/21/2017 02:47:21 AM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Tysen Bair (), tysenbair@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
